fc79d13a2b7d65aca0d993910ef93af2ab5a2e63
Jeniss09 <jenishmevada1@gmail.com>
2023-12-07 17:06:25 +0530

    new 9

---
+++ lib/utils/check_data_status.dart
@@ -0,0 +1,27 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screen/home_screen/provider/home_provider.dart';
+
+
+class NetworkConnection {
+  Connectivity networkConnectivity = Connectivity();
+
+  Future<void> checkConnection(BuildContext context) async {
+    ConnectivityResult result = await networkConnectivity.checkConnectivity();
+    // ignore: use_build_context_synchronously
+    checkStatus(result, context);
+    networkConnectivity.onConnectivityChanged.listen((event) {
+      checkStatus(event, context);
+    });
+  }
+
+  void checkStatus(ConnectivityResult result, BuildContext context) {
+    if (result.index == 3 || result.index == 1) {
+      context.read<HomeProvider>().changeStatus(true);
+    } else {
+      context.read<HomeProvider>().changeStatus(false);
+    }
+  }
+}
--- lib/widget/show_bottom_sheet.dart
+++ lib/widget/show_bottom_sheet.dart
@@ -1,27 +1,23 @@
 import 'package:flutter/material.dart';
 
 // ignore: non_constant_identifier_names
-void ShowBottomSheet(BuildContext context)
-{
-  showModalBottomSheet(context: context, builder: (context) {
-    return BottomSheet(
-      onClosing: () {},
-      builder: (context) => SizedBox(
-        height: 700,
-        child: Column(
-          children: [
-            const SizedBox(height: 50,),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("yes"),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("No"),
-            ),
-          ],
+void ShowBottomSheet(BuildContext context) {
+  showModalBottomSheet(
+    context: context,
+    builder: (context) {
+      return BottomSheet(
+        onClosing: () {},
+        builder: (context) => const SizedBox(
+          height: 700,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+            ],
+          ),
         ),
-      ),
-    );
-  },);
+      );
+    },
+  );
 }
